4d91da87f4006eee6fc99a479dbd355d4ee57e0f
Gabriel Buentello <galfrano@gmail.com>
2023-03-28 22:45:00 +0200

    first screen

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,32 +1,37 @@
+import { useState } from "react";
+
 export default function Index() {
+  const [visible, setVisible] = useState(false)
+
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div className="wrapper">
+      <div className="dog">
+        <h1>Classy.io</h1>
+        <div className="quote">
+        <q>Intelligence is like a four-wheel drive.
+          It allows you to get stuck in more remote places..</q>
+          <hr />
+        <p>Garrison Keillor</p>
+        </div>    
+      </div>
+      <div className="right-side">
+        <div className="right-corner">
+          Don't have an account? <a href="">SIGN UP</a>
+        </div>
+        <div className="sign">
+          <h2>Sign in to Classy</h2>
+          <h3>Enter your details below</h3>
+          <form method="post">
+            <input type="text" placeholder="Email" />
+            <input type={visible ? "text" : "password"} placeholder="Password" />
+            <img
+              src="images/eye.png"
+              onMouseDown={() => setVisible(true)}
+              onMouseUp={() => setVisible(false)} />
+            <button>SIGN IN</button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
