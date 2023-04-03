0e91c5980f23ec70fca9461a9983c2b667c11e7f
Gabriel Buentello <galfrano@gmail.com>
2023-04-03 09:39:51 +0200

    join and leave

--- app/models/attendees.ts
+++ app/models/attendees.ts
@@ -6,7 +6,20 @@ export async function join(classId: any, userId: any){
         body: JSON.stringify(attendee)
       };
       const res = await fetch(
-        'http://api-gen.local/attendee/', requestOptions
-      ).then((res) => {console.log({res, attendee: JSON.stringify(attendee)});return res.json()});
+        'http://api-gen.local/attendees/', requestOptions
+      ).then((res) => res.json());
+      return res;
+}
+
+export async function leave(classId: any, userId: any){
+    const attendee = {"class": classId, "attendee": userId}
+    const requestOptions = {
+        method: 'DELETE',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(attendee)
+      };
+      const res = await fetch(
+        'http://api-gen.local/attendees/', requestOptions
+      ).then((res) => res.json());
       return res;
 }
